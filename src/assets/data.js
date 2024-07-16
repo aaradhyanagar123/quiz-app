a7dbad46c2bb6bfd5a7f4e508d4c188dd314b2d6
Aaradhya Nagar <aaradhyanagar22@gmail.com>
2024-07-16 13:58:19 +0530

    This is my first commit

---
+++ src/assets/data.js
@@ -0,0 +1,46 @@
+export const data = [
+    {
+        question: "Which device is required for the Internet connection?",
+        option1: "Modem",
+        option2: "Router",
+        option3: "Lan Cable",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "Which continent has the highest number of countries?",
+        option1: "Asia",
+        option2: "Europe",
+        option3: "North Africa",
+        option4: "Africa",
+        ans: 4,
+
+    },
+    {
+        question: "Junk e-mail is also called?",
+        option1: "Spam",
+        option2: "Fake",
+        option3: "Archived",
+        option4: "Bin",
+        ans: 1,
+    },
+    {
+        question: "First page of Website is termed as?",
+        option1: "Index Page",
+        option2: "Sitemap",
+        option3: "Homepage",
+        option4: "Pen Drive",
+        ans: 2,
+
+    },
+    {
+        question: "A computer cannot BOOT if it does not have?",
+        option1: "Application Software",
+        option2: "Internet",
+        option3: "Operating System",
+        option4: "Mouse",
+        ans: 3,
+
+
+    },
+]
